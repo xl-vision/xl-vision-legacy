81fa797afa11d9f9e470ed4a9cd881c42ed0d691
Rhys Xia <xrs4433@outlook.com>
2019-01-31 02:40:24 +0800

    :white_check_mark:增加测试用例

--- src/components/grid/test/index.tsx
+++ src/components/grid/test/index.tsx
@@ -1,10 +1,57 @@
 import * as React from 'react'
 import { render } from 'enzyme'
 import Row from '../row'
+import Col from '../col'
 
-describe('row', () => {
-    it('should render row', () => {
-        const wrapper = render(<Row></Row>)
-        expect(wrapper).toMatchSnapshot()
-    })
+describe('栅格系统', () => {
+  it('基本用法', () => {
+    const wrapper = render(
+      <Row>
+        <Col span={4}>Col1</Col>
+        <Col span={5}>Col2</Col>
+        <Col span={7}>Col3</Col>
+        <Col span={8}>Col4</Col>
+      </Row>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('响应式', () => {
+    const wrapper = render(
+      <Row>
+        <Col
+          span={{
+            md: 4,
+            lg: 6
+          }}
+        >
+          Col1
+        </Col>
+        <Col
+          span={{
+            md: 5,
+            lg: 6
+          }}
+        >
+          Col2
+        </Col>
+        <Col
+          span={{
+            md: 7,
+            lg: 6
+          }}
+        >
+          Col3
+        </Col>
+        <Col
+          span={{
+            md: 8,
+            lg: 6
+          }}
+        >
+          Col4
+        </Col>
+      </Row>
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
 })
