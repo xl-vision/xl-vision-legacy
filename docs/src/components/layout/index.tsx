923e39cbb8460792ff2e09391c3e2456bfc5bda8
Rhys Xia <xrs4433@outlook.com>
2019-01-31 20:54:43 +0800

    测试发布功能

--- docs/src/components/layout/index.tsx
+++ docs/src/components/layout/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import Aside from '../aside'
 import Content from '../content'
 import Footer from '../footer'
