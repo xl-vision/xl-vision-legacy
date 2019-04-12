8a4c0dcb2548f5235d99c197b6f3194cdd1441bf
x84103374 <xiaruisong@huawei.com>
2019-04-12 11:51:09 +0800

    :construction_worker:Update travis

--- script/ci/test.sh
+++ script/ci/test.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-npm run test  -- --coverage && bash <(curl -s https://codecov.io/bash) && npm run codacy-coverage
+npm run test  -- --coverage && bash <(curl -s https://codecov.io/bash) && npm run codacy-coverage -- -t ${CODACY_ACCOUNT_TOKEN}
