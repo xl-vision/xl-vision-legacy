5c75aa4c8138b45f9a345dda3964ecab8ec49778
Rhys Xia <xrs4433@outlook.com>
2019-06-25 13:42:46 +0800

    support service work

--- site/index.tsx
+++ site/index.tsx
@@ -2,9 +2,12 @@ import * as React from 'react'
 import * as ReactDom from 'react-dom'
 // import {hot} from 'react-hot-loader/root'
 import Layout from './components/layout'
+import * as ServiceWorker from './serviceWorker'
 
 import './assets/style/index.scss'
 
 // const HotLayout = hot(Layout)
 
 ReactDom.render(<Layout />, document.querySelector('#app'))
+
+ServiceWorker.register()
