098628c9c55ec261ba9d827a805881ed448985aa
Rhys Xia <xrs4433@outlook.com>
2019-08-31 21:42:14 +0800

    format files

--- jest.config.js
+++ jest.config.js
@@ -2,21 +2,11 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   // testEnvironment: 'node',
-  setupFiles: [
-    './script/test/setup.js'
-  ],
-  testMatch: [
-    '**/src/**/__test__/index.ts?(x)'
-  ],
+  setupFiles: ['./script/test/setup.js'],
+  testMatch: ['**/src/**/__test__/index.ts?(x)'],
   // collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/__*__/**',
-    '!src/icon/icons/**'
-  ],
-  snapshotSerializers: [
-    'enzyme-to-json/serializer'
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/__*__/**', '!src/icon/icons/**'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json',
--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,14 @@ const autoprefixer = require('autoprefixer')
 module.exports = {
   plugins: [
     autoprefixer({
-      overrideBrowserslist: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 9', 'iOS >= 8', 'Android >= 4']
+      overrideBrowserslist: [
+        'last 2 versions',
+        'Firefox ESR',
+        '> 1%',
+        'ie >= 9',
+        'iOS >= 8',
+        'Android >= 4'
+      ]
     })
   ]
 }
