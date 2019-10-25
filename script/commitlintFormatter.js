4d635208a9b468a461abdcadce451b2768de2fda
Rhys Xia <xrs4433@outlook.com>
2019-10-25 16:33:25 +0800

    build: show the error tip when commit message format is illegal

--- script/commitlintFormatter.js
+++ script/commitlintFormatter.js
@@ -2,6 +2,9 @@ const formatter = require('@commitlint/format')
 
 module.exports = function(report, options) {
   const isError = report.errorCount > 0
-  const result = formatter.default(report, options)
-  return result + isError && "\n\nPlease use command 'npm run commit' to commit changes\n\n"
+  let result = formatter.default(report, options)
+  if (isError) {
+    result += "\n\nPlease use command 'npm run commit' to commit changes\n\n"
+  }
+  return result
 }
