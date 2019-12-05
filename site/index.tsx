5e312c61ed801296a4387a32d55abad182b0cbb9
Rhys Xia <xrs4433@outlook.com>
2019-12-05 21:46:46 +0800

    chore: update

--- site/index.tsx
+++ site/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import * as ReactDom from 'react-dom'
+import * as ReactDOM from 'react-dom'
 
 import './assets/style/index.scss'
 import Layout from './components/layout'
 // import * as ServiceWorker from './serviceWorker'
 
-ReactDom.render(<Layout />, document.querySelector('#app'))
+ReactDOM.render(<Layout />, document.querySelector('#app'))
 
 // 本地开发不开启pwa
 // process.env.NODE_ENV === 'production' ? ServiceWorker.register() : ServiceWorker.unregister()
