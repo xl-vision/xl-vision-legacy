c3750ff53c5eb0863d05abf0016752da82607ae3
Rhys Xia <xrs4433@outlook.com>
2019-12-22 12:13:15 +0800

    test(modal): add test for ConformModal

---
+++ src/modal/__test__/confirm-modal.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Modal from '..'
+import { act } from 'react-dom/test-utils'
+import { mount } from 'enzyme'
+import wait from '../../../test/wait'
+import ConfirmModal from '../confirm-modal'
+
+describe('ConfirmModal', () => {
+  it('渲染ConfirmModal', () => {
+    const wrapper = mount(<ConfirmModal title={'title'} content={'content'} />)
+
+    expect(wrapper.render()).toMatchSnapshot()
+  })
+
+  it('使用create创建modal', async () => {
+    Modal.create({
+      title: <div className='title'>title</div>,
+      content: 'content'
+    })
+
+    await act(() => {
+      return wait(100)
+    })
+
+    const node = document.querySelector('.xl-modal__wrap')
+
+    expect(node?.parentNode).toBe(document.body)
+
+    expect((node as HTMLDivElement).style.display).toBe('')
+
+    const title = document.querySelector('.title')
+    expect(title?.textContent).toBe('title')
+
+    // todo: 测试框架在更新时会多生成一个div，不准确
+    // modal.update({
+    //   title: <div className='title'>new title</div>
+    // })
+
+    // await act(() => {
+    //   return wait(100)
+    // })
+
+    // expect(title?.textContent).toBe('new title')
+
+    // modal.destroy()
+
+    // await act(() => {
+    //   return wait(100)
+    // })
+
+    // node = document.querySelector('.xl-modal__wrap')
+
+    // console.log(document.body.innerHTML)
+
+    // expect(node).toBeNull()
+  })
+})
