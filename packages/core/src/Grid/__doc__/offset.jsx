165e92063b90531921ee95df3fae1f65a61cb6bd
Rhys Xia <xrs4433@outlook.com>
2020-07-23 21:41:01 +0800

    docs: update

---
+++ packages/core/src/Grid/__doc__/offset.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Grid, createUseStyles } from '@xl-vision/core'
+
+export default () => {
+  const styles = useStyles()
+  return (
+    <div>
+      <div className={styles.root}>
+        <Grid gutter={10}>
+          <Grid.Col span={6}>
+            <div className={styles.box}>col1</div>
+          </Grid.Col>
+          <Grid.Col span={6} offset={6}>
+            <div className={styles.box}>col2</div>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <div className={styles.box}>col3</div>
+          </Grid.Col>
+        </Grid>
+      </div>
+      <div className={styles.root}>
+        <Grid gutter={10}>
+          <Grid.Col span={6}>
+            <div className={styles.box}>col1</div>
+          </Grid.Col>
+          <Grid.Col span={6} push={6}>
+            <div className={styles.box}>col2</div>
+          </Grid.Col>
+          <Grid.Col span={6} pull={6}>
+            <div className={styles.box}>col3</div>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <div className={styles.box}>col4</div>
+          </Grid.Col>
+        </Grid>
+      </div>
+      <div className={styles.root}>
+        <Grid gutter={10}>
+          <Grid.Col span={6} offset={6}>
+            <div className={styles.box}>col1</div>
+          </Grid.Col>
+          <Grid.Col span={6} pull={12}>
+            <div className={styles.box}>col2</div>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <div className={styles.box}>col3</div>
+          </Grid.Col>
+        </Grid>
+      </div>
+    </div>
+  )
+}
+
+const useStyles = createUseStyles((theme) => {
+  return {
+    root: {
+      padding: '0.5rem',
+      backgroundColor: theme.color.getContrastColor().divider,
+      borderRadius: '4px',
+
+      '& + &': {
+        marginTop: '1rem'
+      }
+    },
+    box: {
+      textAlign: 'center',
+      padding: '0.5rem',
+      backgroundColor: theme.color.themes.primary,
+      color: theme.color.getContrastColor(theme.color.themes.primary).text.primary,
+      borderRadius: '4px'
+    }
+  }
+})
